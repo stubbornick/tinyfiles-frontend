a14f7c24e6b140d81620e9ced609cd96f43562e6
subbornick <38818331+stubbornick@users.noreply.github.com>
2020-07-18 23:19:17 +0200

    Laid out basic header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,33 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="border-bottom">
+        <h1>Tinyfiles</h1>
       </header>
+
+      <footer className="pt-2 my-md-3 pt-md-3 border-top">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6">
+              <img className="mb-2" src={logo} alt="" width="24" height="24"></img>
+              <small className="d-block mb-3 text-muted">&copy; 2020</small>
+            </div>
+            <div className="col-md-6">
+                <h5>About</h5>
+                <ul className="list-unstyled text-muted">
+                  <li>
+                    Icon made by
+                    <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a>.
+                  </li>
+                  <li>
+                    App made by
+                    <a href="https://github.com/stubbornick" title="stubbornick">stubbornick</a>.
+                  </li>
+                </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
