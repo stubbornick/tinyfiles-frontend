ede284453622ac2b8a82bd76c13fe11604d4eb82
subbornick <38818331+stubbornick@users.noreply.github.com>
2020-08-22 12:02:19 +0200

    Remove 'refresh' button

--- src/FileModule/FileModule.tsx
+++ src/FileModule/FileModule.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { RefreshCcw } from 'react-feather';
 
 import './FileModule.css';
 import { FileTable } from './FileTable';
@@ -10,12 +9,6 @@ export function FileModule() {
     <div className="pt-2 my-md-3 p-md-3">
       <div className="d-flex justify-content-start">
         <h4 className="d-flex px-2 text-muted">Uploaded files</h4>
-        <button
-          className="btn btn-inline btn-outline-secondary"
-          title="Refresh list"
-        >
-          <RefreshCcw size={18} />
-        </button>
         <FileUploader />
       </div>
       <FileTable />
